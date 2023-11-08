93a8563debc90f23c8eeb619ccbc8fdddee4ce55
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-11-08 12:46:42 +0330

    Add Concurrency

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,1 +1,25 @@
 package concurrency
+
+type WebsiteChecker func(string) bool
+type Result struct {
+	string
+	bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	result := make(map[string]bool)
+	resultChannel := make(chan Result)
+
+	for _, url := range urls {
+		go func(url string) {
+			resultChannel <- Result{url, wc(url)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		result[r.string] = r.bool
+	}
+
+	return result
+}
